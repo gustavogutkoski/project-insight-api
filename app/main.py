4694d56ac31b6217cf5564209a9bd2b9594882a8
Gustavo A. Gutkoski <gustavogutkoski2609@gmail.com>
2025-04-28 11:02:07 -0300

    feat: initialize project

---
+++ app/main.py
@@ -0,0 +1,6 @@
+from fastapi import FastAPI
+from app.api.v1.endpoints import router as api_router
+
+app = FastAPI(title="Project Insight API - MVP")
+
+app.include_router(api_router, prefix="/api/v1")
