4f52bae7b829fe087794e274dd24ca0593ad34ef
Gustavo A. Gutkoski <gustavogutkoski2609@gmail.com>
2025-09-26 09:33:40 -0300

    fix: typecheck

--- app/api/v1/endpoints.py
+++ app/api/v1/endpoints.py
@@ -1,8 +1,9 @@
+from typing import Dict
 from fastapi import APIRouter
 
 router = APIRouter()
 
 
 @router.get("/healthcheck")
-def healthcheck():
+def healthcheck() -> Dict[str, str]:
     return {"status": "ok"}
